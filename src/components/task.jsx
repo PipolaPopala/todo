9229d307c39d769c79f3a56689aa83620748f216
Evgeny Lepekho <lepehun@mail.ru>
2023-10-10 12:28:36 +0200

    feat: good eslint without prettier, babel, husky

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,51 +1,48 @@
-import React, {Component} from'react';
-import { formatDistanceToNow } from 'date-fns';
-import KG from 'date-fns/locale/en-AU';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import KG from 'date-fns/locale/en-AU'
+import PropTypes from 'prop-types'
+
+var gggggggg = dfefwf
 
 export default class Task extends Component {
+  render() {
+    const { desc, id, onEditForm, onDeleted, onToggleCompleted, date } = this.props
 
-    render() {
-
-        const {desc, id, onEditForm, onDeleted, onToggleCompleted, date} = this.props;
-
-        return (
-            <div className="view">
-                <input id={id} className="toggle" type="checkbox"
-                onChange={onToggleCompleted}/>
-                <label htmlFor={id}>
-                    <span className="description">{desc}</span>
-                    <span className="created">
-                    {`created ${formatDistanceToNow(date, {
-                    includeSeconds: true,
-                    locale: KG,
-                    addSuffix: true,
-                    })}`}
-                    </span>
-                </label>
-                <button className="icon icon-edit"
-                onClick={onEditForm}></button>
-                <button className="icon icon-destroy"
-                onClick={onDeleted}></button>
-            </div>
-        );
-    };
-};
+    return (
+      <div className="view">
+        <input id={id} className="toggle" type="checkbox" onChange={onToggleCompleted} />
+        <label htmlFor={id}>
+          <span className="description">{desc}</span>
+          <span className="created">
+            {`created ${formatDistanceToNow(date, {
+              includeSeconds: true,
+              locale: KG,
+              addSuffix: true,
+            })}`}
+          </span>
+        </label>
+        <button className="icon icon-edit" onClick={onEditForm}></button>
+        <button className="icon icon-destroy" onClick={onDeleted}></button>
+      </div>
+    )
+  }
+}
 
 Task.defaultProps = {
-    desc: 'doing something!!!',
-    id: Math.floor(Math.random() * (10000 - 1000 + 1) + 1000),
-    onEditForm:() => {},
-    onDeleted: () => {},
-    onToggleCompleted: () => {},
-    date: new Date()
-};
+  desc: 'doing something!!!',
+  id: Math.floor(Math.random() * (10000 - 1000 + 1) + 1000),
+  onEditForm: () => {},
+  onDeleted: () => {},
+  onToggleCompleted: () => {},
+  date: new Date(),
+}
 
 Task.propTypes = {
-    desc: PropTypes.string,
-    id: PropTypes.number,
-    onEditForm: PropTypes.func,
-    onDeleted: PropTypes.func,
-    onToggleCompleted: PropTypes.func,
-    date: PropTypes.instanceOf(Date)
-};
+  desc: PropTypes.string,
+  id: PropTypes.number,
+  onEditForm: PropTypes.func,
+  onDeleted: PropTypes.func,
+  onToggleCompleted: PropTypes.func,
+  date: PropTypes.instanceOf(Date),
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,168 +1,193 @@
+import React, { Component } from 'react'
+import { createRoot } from 'react-dom/client'
+import PropTypes from 'prop-types'
 
-import React, {Component} from'react';
-import { createRoot } from 'react-dom/client';
-import PropTypes from 'prop-types';
+import './components/style.css'
 
-import './components/style.css';
+import NewTaskForm from './components/new-task-form'
+import TaskList from './components/task-list'
 
-import NewTaskForm from './components/new-task-form';
-import TaskList from './components/task-list';
-
+var gggg = fghj
 
 class App extends Component {
+  maxId = 100
 
-  maxId = 100;
-
-  state = {
-    todoData: [
-      {desc: 'Completed task', completed:false, editing:false, hidden:false, date:new Date(), id: 1},
-      {desc: 'Editing task', completed:false, editing:false, hidden:false, date:new Date(), id: 2},
-      {desc: 'Active task', completed:false, editing:false, hidden:false, date:new Date(), id: 3},
-    ],
-    filter: 'All',
-  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      todoData: [
+        {
+          desc: 'Completed task',
+          completed: false,
+          editing: false,
+          hidden: false,
+          date: new Date(),
+          id: 1,
+        },
+        {
+          desc: 'Editing task',
+          completed: false,
+          editing: false,
+          hidden: false,
+          date: new Date(),
+          id: 2,
+        },
+        {
+          desc: 'Active task',
+          completed: false,
+          editing: false,
+          hidden: false,
+          date: new Date(),
+          id: 3,
+        },
+      ],
+      filter: 'All',
+    }
+  }
 
   editItemForm = (id) => {
-    this.setState(({todoData}) => {
-      const idx = todoData.findIndex((el) => el.id === id);
-      const oldItem = todoData[idx];
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+      const oldItem = todoData[idx]
       if (!oldItem.completed) {
-        const newItem = {...oldItem, editing:true};
-        const newTodoData = 
-        [...todoData.slice(0, idx), 
-          newItem,
-        ...todoData.slice(idx + 1)];
-        return {todoData: newTodoData}
+        const newItem = { ...oldItem, editing: true }
+        const newTodoData = [...todoData.slice(0, idx), newItem, ...todoData.slice(idx + 1)]
+        return { todoData: newTodoData }
       }
     })
   }
 
   onEdit = (e, id) => {
     if (e.keyCode === 13) {
-      this.setState(({todoData}) => {
-        const idx = todoData.findIndex((el) => el.id === id);
-        const oldItem = todoData[idx];
-        const newItem = {...oldItem, desc: e.target.value, editing:false};
-        const newTodoData = 
-        [...todoData.slice(0, idx), 
-          newItem,
-        ...todoData.slice(idx + 1)];
-        return {todoData: newTodoData}
-      });
-    };
-  };
+      this.setState(({ todoData }) => {
+        const idx = todoData.findIndex((el) => el.id === id)
+        const oldItem = todoData[idx]
+        const newItem = { ...oldItem, desc: e.target.value, editing: false }
+        const newTodoData = [...todoData.slice(0, idx), newItem, ...todoData.slice(idx + 1)]
+        return { todoData: newTodoData }
+      })
+    }
+  }
 
   deleteItem = (id) => {
-    this.setState(({todoData}) => {
-      const idx = todoData.findIndex((el) => el.id === id);
-      const newTodoData = 
-      [...todoData.slice(0, idx), 
-      ...todoData.slice(idx + 1)];
-      return {todoData: newTodoData}
-    });
-  };
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+      const newTodoData = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)]
+      return { todoData: newTodoData }
+    })
+  }
 
   addItem = (e) => {
     if (e.keyCode === 13) {
-      const newItem = {desc: e.target.value, completed:false, editing:false, hidden:false, date:new Date(), id: this.maxId++};
-      this.setState(({todoData}) => {
-        const newTodoData = [...todoData, newItem];
-        return {todoData: newTodoData}
-      });
-      e.target.value = '';
-    };
-  };
+      const newItem = {
+        desc: e.target.value,
+        completed: false,
+        editing: false,
+        hidden: false,
+        date: new Date(),
+        id: this.maxId++,
+      }
+      this.setState(({ todoData }) => {
+        const newTodoData = [...todoData, newItem]
+        return { todoData: newTodoData }
+      })
+      e.target.value = ''
+    }
+  }
 
   onToggleCompleted = (id) => {
-    this.setState(({todoData}) => {
-      const idx = todoData.findIndex((el) => el.id === id);
-      const oldItem = todoData[idx];
-      const newItem = {...oldItem, completed:!oldItem.completed};
-      const newTodoData = 
-      [...todoData.slice(0, idx), 
-        newItem,
-      ...todoData.slice(idx + 1)];
-      return {todoData: newTodoData}
-    });
-  };
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+      const oldItem = todoData[idx]
+      const newItem = { ...oldItem, completed: !oldItem.completed }
+      const newTodoData = [...todoData.slice(0, idx), newItem, ...todoData.slice(idx + 1)]
+      return { todoData: newTodoData }
+    })
+  }
 
   onClearCompleted = () => {
-    this.setState(({todoData}) => {
-      const newTodoData = todoData.filter((el) =>!el.completed);
-      return {todoData: newTodoData}
-    });
-  };
+    this.setState(({ todoData }) => {
+      const newTodoData = todoData.filter((el) => !el.completed)
+      return { todoData: newTodoData }
+    })
+  }
 
   changeFilter = (data) => {
-    this.setState({filter: data});
-  };
-  
+    this.setState({ filter: data })
+  }
+
   filterItems = () => {
-    const {filter} = this.state;
-      switch (filter) {
-        case 'Active': 
-        this.setState(({todoData}) => {
+    const { filter } = this.state
+    switch (filter) {
+      case 'Active':
+        this.setState(({ todoData }) => {
           const newTodoData = todoData.map((el) => {
-            if (el.completed === true) {el.hidden = true}
-            else {el.hidden = false}
+            if (el.completed === true) {
+              el.hidden = true
+            } else {
+              el.hidden = false
+            }
             return el
-          });
-          return {todoData: newTodoData}
-        });
-        break;
-        case 'Completed': 
-        this.setState(({todoData}) => {
+          })
+          return { todoData: newTodoData }
+        })
+        break
+      case 'Completed':
+        this.setState(({ todoData }) => {
           const newTodoData = todoData.map((el) => {
-            if (el.completed === true) {el.hidden = false}
-            else {el.hidden = true}
+            if (el.completed === true) {
+              el.hidden = false
+            } else {
+              el.hidden = true
+            }
             return el
-          });
-          return {todoData: newTodoData}
-        });
-        break;
-        default: 
-        this.setState(({todoData}) => {
+          })
+          return { todoData: newTodoData }
+        })
+        break
+      default:
+        this.setState(({ todoData }) => {
           const newTodoData = todoData.map((el) => {
             el.hidden = false
             return el
-          });
-          return {todoData: newTodoData}
-        });
-      }
-  };
+          })
+          return { todoData: newTodoData }
+        })
+    }
+  }
 
   render() {
-
-    const countTodo = this.state.todoData.filter((el) => !el.completed).length;
+    const countTodo = this.state.todoData.filter((el) => !el.completed).length
 
     return (
       <section className="todoapp">
         <NewTaskForm onAdded={this.addItem} />
-        <TaskList todos={this.state.todoData}
-        onEditForm={this.editItemForm}
-        onEdit={this.onEdit}
-        onDeleted={this.deleteItem}
-        onToggleCompleted={this.onToggleCompleted} 
-        countTodo={countTodo}
-        filter={this.state.filter}
-        changeFilter={this.changeFilter.bind(this)}
-        filterItems={this.filterItems}
-        onClearCompleted={this.onClearCompleted}
+        <TaskList
+          todos={this.state.todoData}
+          onEditForm={this.editItemForm}
+          onEdit={this.onEdit}
+          onDeleted={this.deleteItem}
+          onToggleCompleted={this.onToggleCompleted}
+          countTodo={countTodo}
+          filter={this.state.filter}
+          changeFilter={this.changeFilter.bind(this)}
+          filterItems={this.filterItems}
+          onClearCompleted={this.onClearCompleted}
         />
-        </section>
-    );
-  };
-};
+      </section>
+    )
+  }
+}
 
 App.defaultProps = {
   todoData: [],
   filter: 'All',
-};
+}
 
 App.propTypes = {
   todoData: PropTypes.array,
   filter: PropTypes.string,
-};
+}
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
