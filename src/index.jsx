6c3aa47d4053912029c20ffe27166f64e9beccde
Evgeny Lepekho <lepehun@mail.ru>
2023-10-11 15:57:56 +0200

    feat: add sass

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 
 import App from './components/app/App'
 
-import './components/style.css'
+import './components/style.scss'
 
 const root = createRoot(document.getElementById('root'))
 root.render(<App />)
