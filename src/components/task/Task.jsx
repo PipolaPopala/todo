0a0d5bb7c046daf75e83044e440edf4a14473bba
Evgeny Lepekho <lepehun@mail.ru>
2023-10-30 11:47:04 +0200

    feat: all hooks without working timer

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,89 +1,41 @@
 import { formatDistanceToNow } from 'date-fns'
 import KG from 'date-fns/locale/en-AU'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
 
-// function Task({ desc, id, onEditForm, onDeleted, onToggleCompleted, date, onPlay, onPause, minutes, seconds }) {
-//   return (
-//     <div className="view">
-//       <input id={id} className="toggle" type="checkbox" onChange={onToggleCompleted} />
-//       <label htmlFor={id}>
-//         <span className="title">{desc}</span>
-//         <span className="description">
-//           <button className="icon icon-play" onClick={onPlay}></button>
-//           <button className="icon icon-pause" onClick={onPause}></button>
-//           {`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}
-//         </span>
-//         <span className="description">
-//           {`created ${formatDistanceToNow(date, {
-//             includeSeconds: true,
-//             locale: KG,
-//             addSuffix: true,
-//           })}`}
-//         </span>
-//       </label>
-//       <button className="icon icon-edit" onClick={onEditForm}></button>
-//       <button className="icon icon-destroy" onClick={onDeleted}></button>
-//     </div>
-//   )
-// }
-
-// export default Task
-
-// export default class Task extends Component {
-//   render() {
-//     const { desc, id, onEditForm, onDeleted, onToggleCompleted, date, onPlay, onPause, minutes, seconds } = this.props
-//     return (
-//       <div className="view">
-//         <input id={id} className="toggle" type="checkbox" onChange={onToggleCompleted} />
-//         <label htmlFor={id}>
-//           <span className="title">{desc}</span>
-//           <span className="description">
-//             <button className="icon icon-play" onClick={onPlay}></button>
-//             <button className="icon icon-pause" onClick={onPause}></button>
-//             {`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}
-//           </span>
-//           <span className="description">
-//             {`created ${formatDistanceToNow(date, {
-//               includeSeconds: true,
-//               locale: KG,
-//               addSuffix: true,
-//             })}`}
-//           </span>
-//         </label>
-//         <button className="icon icon-edit" onClick={onEditForm}></button>
-//         <button className="icon icon-destroy" onClick={onDeleted}></button>
-//       </div>
-//     )
-//   }
-// }
-
-export default class Task extends Component {
-  render() {
-    const { desc, id, onEditForm, onDeleted, onToggleCompleted, date, onPlay, onPause, minutes, seconds } = this.props
-    return (
-      <div className="view">
-        <input id={id} className="toggle" type="checkbox" onChange={onToggleCompleted} />
-        <label htmlFor={id}>
-          <span className="title">{desc}</span>
-          <span className="description">
-            <button className="icon icon-play" onClick={onPlay}></button>
-            <button className="icon icon-pause" onClick={onPause}></button>
-            {`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}
-          </span>
-          <span className="description">
-            {`created ${formatDistanceToNow(date, {
-              includeSeconds: true,
-              locale: KG,
-              addSuffix: true,
-            })}`}
-          </span>
-        </label>
-        <button className="icon icon-edit" onClick={onEditForm}></button>
-        <button className="icon icon-destroy" onClick={onDeleted}></button>
-      </div>
-    )
-  }
+export default function Task({
+  desc,
+  id,
+  onEditForm,
+  onDeleted,
+  onToggleCompleted,
+  date,
+  onPlay,
+  onPause,
+  minutes,
+  seconds,
+}) {
+  return (
+    <div className="view">
+      <input id={id} className="toggle" type="checkbox" onChange={onToggleCompleted} />
+      <label htmlFor={id}>
+        <span className="title">{desc}</span>
+        <span className="description">
+          <button className="icon icon-play" onClick={onPlay}></button>
+          <button className="icon icon-pause" onClick={onPause}></button>
+          {`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`}
+        </span>
+        <span className="description">
+          {`created ${formatDistanceToNow(date, {
+            includeSeconds: true,
+            locale: KG,
+            addSuffix: true,
+          })}`}
+        </span>
+      </label>
+      <button className="icon icon-edit" onClick={onEditForm}></button>
+      <button className="icon icon-destroy" onClick={onDeleted}></button>
+    </div>
+  )
 }
 
 Task.defaultProps = {
